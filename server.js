e5f3cf275e8c470caf1a88ec732f9b5c5bf9a778
winkie224 <leanthony104@yahoo.com>
2018-12-23 23:55:44 -0600

    more routes

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("./public"));
+//require("./app/routes/html-routes.js")(app);
 
 // Connect to the Mongo DB
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
@@ -37,7 +38,14 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 // A GET route for scraping the echoJS website
 //const resultArray = [];
 
+app.get("/", function (req, res) {
+  res.render("index", { scrape: "Scrape!" });
+});
+
 app.get("/scrape", function (req, res) {
+  db.Article.remove({}, function(err) {
+    console.log("Collection Removed");
+  });
   axios.get("http://editorial.rottentomatoes.com/news/").then(function (response) {
     const $ = cheerio.load(response.data);
     $("div.newsItem").each(function (i, element) {
@@ -55,11 +63,11 @@ app.get("/scrape", function (req, res) {
         const summary = cheerio.load(response.data)("div.articleContentBody").children().first().text();
         result.title = title;
         result.link = link;
-          if (summary === "") {
-            result.summary = "No Summary Available."
-          } else {
-            result.summary = summary;
-          }
+        if (summary === "") {
+          result.summary = "No Summary Available."
+        } else {
+          result.summary = summary;
+        }
         console.log(result);
         db.Article.create(result)
           .then(function (dbArticle) {
@@ -70,55 +78,55 @@ app.get("/scrape", function (req, res) {
           });
       });
     });
-    res.send("Scrape Complete");
   });
+  res.render("index", { scrape: "Scrape Complete" });
 });
 
 //Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+app.get("/articles", function (req, res) {
   // Grab every document in the Articles collection
   db.Article.find({})
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 //Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.Article.findOne({ _id: req.params.id })
     // ..and populate all of the notes associated with it
     .populate("note")
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
   // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
-    .then(function(dbNote) {
+    .then(function (dbNote) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
